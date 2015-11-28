266cab714b804ab509899ecf5fdfbb4b0fc8e908
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2015-11-28 16:10:48 -0500

    Additional changes to Assignemnt 4

--- Assignments/Assignment 4/buildCLASS.sh
+++ Assignments/Assignment 4/buildCLASS.sh
@@ -3,5 +3,6 @@ for i
 do
   filename="${i%.*}"
 	java -jar umple_1.22.0.5146.jar -g GvClassDiagram $filename.ump
-	dot $filename.gv -Tpdf -o $filename.pdf
+	extension="cd.gv"
+	dot $filename$extension -Tpdf -o $filename.pdf
 done
